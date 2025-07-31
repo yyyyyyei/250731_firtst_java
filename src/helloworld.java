a88ab02e0ee8f559b6280b05379241492cfa2ebe
yyyyyyei <ye0126jin@naver.com>
2025-07-31 11:07:49 +0900

    Initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,15 @@
+public class helloworld {
+    // 진입점 : 이 java 파일을 실행했을 때 이 구문을 찾아서 실행시킴
+    // main 약어 사용
+    public static void main(String[] args) {
+        // sout 약어 사용
+        // System.out.println("{본인이 출력하고 하는 내용}"); 작은따옴표 안됨!
+        // System.out.print() -> 줄바꿈 x
+        System.out.println("hello world!");
+        // 같은 줄 복사 : Ctrl + D
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        // 한 줄 삭제 : Ctrl + Y
+    }
+}
